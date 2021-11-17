2861920c50b6449e1bdf865782656c678af86fde
Marcin Pawlowski <marcin.pawlowski@microsoft.wsei.edu.pl>
2021-11-17 18:49:48 +0100

    Add project files.

---
+++ Marcin Pawłowski Lab3 ZD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Marcin_Pawłowski_Lab3_ZD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
