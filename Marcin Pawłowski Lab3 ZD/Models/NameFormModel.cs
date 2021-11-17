4faf7162d2f1c7cb4d1f1e273711d7cecd4395f6
Marcin Pawlowski <marcin.pawlowski@microsoft.wsei.edu.pl>
2021-11-17 19:51:54 +0100

    Add: Model for handling form input

---
+++ Marcin Pawłowski Lab3 ZD/Models/NameFormModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Marcin_Pawłowski_Lab3_ZD.Models
+{
+    public class NameFormModel
+    {
+        private const string polishname = "Imię";
+
+        [Display(Name = polishname)]
+        public string Name { get; set; }
+        [Required]
+        public int Counter { get; set; }
+        [Required]
+
+        private const string polishCounter = "Liczba powtórzeń";
+
+        [Display(Name = polishCounter)]
+        public int Count
+        {
+            get
+            {
+                return Counter += 1;
+            }
+        }
+}
